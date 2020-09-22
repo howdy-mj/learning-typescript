8ee36414460d50e90b72f406e9bb5048f938705b
kmj <hi.minjungkim@gmail.com>
2020-09-22 21:14:11 +0900

    Setting Google Maps Type Definition

--- 03-maps/src/index.ts
+++ 03-maps/src/index.ts
@@ -1,8 +1,11 @@
-import { User } from './User';
-import { Company } from './Company';
+/// <reference types="@types/googlemaps" />
+// import { User } from './User';
+// import { Company } from './Company';
 
-const user = new User();
-console.log(user);
-
-const company = new Company();
-console.log(company);
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
