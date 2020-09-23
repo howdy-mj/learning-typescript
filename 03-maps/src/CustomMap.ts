95560daf64bf67fb1072dcf68d20460dc66fe1d9
kmj <hi.minjungkim@gmail.com>
2020-09-23 22:52:03 +0900

    Private Modifiers, Adding Markers

---
+++ 03-maps/src/CustomMap.ts
@@ -0,0 +1,34 @@
+// Instructions to every other class
+// on how they can be an argument to 'addMarker'
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
+export class CustomMap {
+  // 다른 개발자가 직접 googlemaps의 메소드에 접근하지 못하도록 private으로 설정
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    // 어느 클래스든 Mappable 구조와 같다면 사용 가능
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+  }
+}
